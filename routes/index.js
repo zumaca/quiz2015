3bdba59b7f7d4b4d3d8090fd5fa1ac44ca55de7a
ezumaquero <ezumaquero@localhost.localdomain>
2015-07-08 18:22:04 +0200

    Practica modulo 6

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var authorController = require('../controllers/author_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,4 +10,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question' , quizController.question);
 router.get('/quizes/answer' , quizController.answer);
+router.get('/author' , authorController.author);
+
 module.exports = router;
