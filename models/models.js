30fbc1e8d9fcfaceb7cab17b24fb36b6982dc61c
Emilio Zumaquero <ezumaquero@localhost.localdomain>
2015-07-20 20:33:44 +0200

    p2p modulo8

--- models/models.js
+++ models/models.js
@@ -1,7 +1,7 @@
 
 var path = require('path');
 
-//POTSGRES DATABASE_URL= postgres://ftevyueftavlma:P2OFrjXzyzqw7sMkZLTPqY6OoJ@ec2-54-204-15-41.compute-1.amazonaws.com:5432/dfccu0aveq59o6
+//POTSGRES DATABASE_URL=  postgres://kqlphqyoyxozrb:kpvXSTkyLJgF8nx6W0rEmoZRG8@ec2-54-83-20-177.compute-1.amazonaws.com:5432/dfgd4qipt7hml2
 
 //LOCAL DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
